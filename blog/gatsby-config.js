e8686e8b35dde5dab8a6208f56ecaeb59107248f
jerry-janacek <janacej1@gmatc.matc.edu>
2025-02-15 14:37:57 -0600

    secured api keys

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -4,6 +4,10 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
  */
 
+require('dotenv').config({
+  path:`.env.${process.env.NODE_ENV}`
+}) 
+
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
@@ -24,7 +28,7 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: `${process.env.SPACE_ID}`,
-        accessToken: `${process.env.ACESS_TOKEN}`
+        accessToken: `${process.env.ACCESS_TOKEN}`
       }
     },
     `gatsby-plugin-image`,
@@ -35,6 +39,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
